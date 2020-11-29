19aa09a86cdf682fd18efed06478b713794255a2
SpermLord <hiimnghia.real@gmail.com>
2020-11-29 23:30:31 +0700

    npm init

---
+++ README.md
@@ -0,0 +1,22 @@
+# Convertv2
+
+> This project will convert a command from Mirai v1 to Mirai v2
+> WIP
+
+## How To Use
+
+- Open cmd in this project folder.
+
+- Type this in cmd:
+```cmd
+mklink /d "v1" "<Path To V1>"
+```
+
+Replace <Path To V1> with the actual path. Eg: C:\Users\SpermLord\Desktop\mirai
+
+- Use:
+```
+node index <command>
+```
+
+Eg: ```node index help```
--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const fs = require('fs-extra')
 var args = process.argv.slice(2)
+if (args.length == 0) return console.error('Enter a command that need to be ported.')
 if (args.length != 1) return console.error('Only accept 1 argument.')
-if (args[0] == '--force') isForce = true
+if (!fs.existsSync('v1/app')) return console.error('This file need to be placed in v1 folder.')
 
 //Get language data (default is Vietnamese)
-var langFile = (fs.readFileSync(`./app/handle/src/langs/vi.lang`, { encoding: 'utf-8' })).split(/\r?\n/)
+var langFile = (fs.readFileSync(`v1/app/handle/src/langs/vi.lang`, { encoding: 'utf-8' })).split(/\r?\n/)
 var langData = langFile.filter(item => item.indexOf('#') != 0 && item != '')
 
-if (!fs.existsSync('app')) console.error('This file need to be placed in v1 folder.')
-
-var message.js = fs.readFileSync(`./app/handle/message.js`, { encoding: 'utf-8' })
+var message = fs.readFileSync(`v1/app/handle/message.js`, { encoding: 'utf-8' })
+if (message.includes(`if (contentMessage.indexOf(\`\$\{prefix\}${args[0]}\`) == 0)`)) {
+	let index = message.indexOf(`if (contentMessage.indexOf(\`\$\{prefix\}${args[0]}\`) == 0)`)
+	message = message.slice(index, message.length)
+	let getComment = message.indexOf('//')
+	message = message.slice(0, getComment)
+	console.log(message)
+}
+else console.error('No')

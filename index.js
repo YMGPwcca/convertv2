588979c20b8223da96f4648c82979a8042a26181
SpermLord <hiimnghia.real@gmail.com>
2020-11-30 14:04:24 +0700

    0.0.2
    Remake to CLI

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const fs = require('fs-extra')
-var args = process.argv.slice(2)
-if (args.length == 0) return console.error('Enter a command that need to be ported.')
-if (args.length != 1) return console.error('Only accept 1 argument.')
-if (!fs.existsSync('v1/app')) return console.error('This file need to be placed in v1 folder.')
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const files = require('./lib/files'); //Useless for now
+const getCmd = require('./lib/getCmd');
+
+//Clear terminal/command promt interface and print Convertv2
+clear();
+console.log(chalk.yellow(figlet.textSync('Convertv2', { horizontalLayout: 'full' })));
 
 //Get language data (default is Vietnamese)
 var langFile = (fs.readFileSync(`v1/app/handle/src/langs/vi.lang`, { encoding: 'utf-8' })).split(/\r?\n/)
 var langData = langFile.filter(item => item.indexOf('#') != 0 && item != '')
 
-var message = fs.readFileSync(`v1/app/handle/message.js`, { encoding: 'utf-8' })
-if (message.includes(`if (contentMessage.indexOf(\`\$\{prefix\}${args[0]}\`) == 0)`)) {
-	let index = message.indexOf(`if (contentMessage.indexOf(\`\$\{prefix\}${args[0]}\`) == 0)`)
-	message = message.slice(index, message.length)
-	let getComment = message.indexOf('//')
-	message = message.slice(0, getComment)
-	console.log(message)
-}
-else console.error('No')
+//Do the thing
+(async () => {
+  const credentials = await getCmd.askCmdInfo();
+  console.log(credentials);
+})();

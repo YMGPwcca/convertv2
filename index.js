326ccc7e1d6e817ea7da1a1379088c177bfc2c86
SpermLord <hiimnghia.real@gmail.com>
2020-12-03 14:41:04 +0700

    0.0.4

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ const clear = require('clear');
 const figlet = require('figlet');
 const fs = require('fs-extra');
 const beauty = require('js-beautify').js;
-const files = require('./lib/files'); //Useless for now
-const getCmd = require('./lib/getCmd');
+const getCmd = require('./lib/cmd');
+const getText = require('./lib/text');
 
 //Clear terminal/command promt interface and print Convertv2
 clear();
 console.log(chalk.yellow(figlet.textSync('Convertv2', { horizontalLayout: 'full' })));
 console.log(chalk.cyan('> By SpermLord'));
 
-//Get language data (default is Vietnamese)
-var langFile = (fs.readFileSync(`v1/app/handle/src/langs/vi.lang`, { encoding: 'utf-8' })).split(/\r?\n/);
-var langData = langFile.filter(item => item.indexOf('#') != 0 && item != '');
+//Create dist folder
+fs.ensureDirSync('dist');
+
+//message.js
 var message = fs.readFileSync('v1/app/handle/message.js', { encoding: 'utf-8' });
 var helpInfo = JSON.parse(fs.readFileSync('v1/app/handle/src/help/listCommands.json'));
 
@@ -38,30 +39,37 @@ info: ${info}
 
 //Do the thing
 let init = async () => {
-  const credentials = await getCmd.askCmdInfo();
-  let { name, version, perm, credit, cooldown, dependencies } = credentials;
-  if (message.includes(`if (contentMessage.indexOf(\`\$\{prefix\}${name}\`) == 0)`)) {
-		let index = message.indexOf(`if (contentMessage.indexOf(\`\$\{prefix\}${name}\`) == 0)`);
-		message = message.slice(index, message.length);
-		let getComment = message.indexOf('//');
-		message = message.slice(0, getComment);
-		let start = message.indexOf(') {') + 3 || message.indexOf('){') + 2;
-		let end = message.lastIndexOf('}');
-		message = message.slice(start, end);
-		message = message.replace(/senderID/g, 'event.senderID');
-		message = message.replace(/, threadID/g, ', event.threadID');
-		message = message.replace(/, messageID/g, ', event.messageID');
-		message = message.replace(/\t/g,'');
-		message = message.replace(/contentMessage.slice\(prefix.length \+ \d, contentMessage.length\)/, 'args.join(" ")');
+	const credentials = await getCmd.askCmdInfo(message);
+	let { name, version, perm, credit, cooldown, dependencies } = credentials;
+	let index = message.indexOf(`if (contentMessage.indexOf(\`\$\{prefix\}${name}\`) == 0)`);
+	message = message.slice(index, message.length);
+	let getComment = message.indexOf('//');
+	message = message.slice(0, getComment);
+	let start = message.indexOf(') {') + 3 || message.indexOf('){') + 2;
+	let end = message.lastIndexOf('}');
+	message = message.slice(start, end);
+	message = message.replace(/senderID/g, 'event.senderID').replace(/, threadID/g, ', event.threadID').replace(/, messageID/g, ', event.messageID').replace(/\t/g,'').replace(/contentMessage.slice\(prefix.length \+ \d, contentMessage.length\)/, 'args.join(" ")');
 
-		let cmdInfo = helpInfo.filter(item => item.name == name)[0];
-		let { desc, usage, group } = cmdInfo;
-		let info = '[]';
-		let splitDependencies = dependencies.split(', ');
-		if (splitDependencies.length > 0 && splitDependencies[0] != '') dependencies = JSON.stringify(splitDependencies);
-		else dependencies = '[]';
-		let fullCode = beauty(config(name, version, perm, credit, desc, group, usage, cooldown, dependencies, info) + '\n\n' + run(message), { indent_size: 2, space_in_empty_paren: true })
-		fs.writeFileSync(`${name}.js`, fullCode);
+	//Get cmd info
+	let cmdInfo = helpInfo.filter(item => item.name == name)[0];
+	let { desc, usage, group } = cmdInfo;
+	let splitDependencies = dependencies.split(', ');
+	if (splitDependencies.length > 0 && splitDependencies[0] != '') dependencies = JSON.stringify(splitDependencies);
+	else dependencies = '[]';
+
+	//Reverse getText function
+	let a = message.match(/api.sendMessage\((.*?)ID\)/g);
+	for (let i of a) {
+		let o = i;
+		i = i.replace(/api\.sendMessage\(/g, '').replace(/, event\.threadID, event\.messageID\)/g, '').replace(/getText\(/g, '');
+		i = i.slice(0, -1);
+		let send = (text) => `api.sendMessage(\`${text}\`, event.threadID, event.messageID)`;
+		message = message.replace(o, send(getText(i)));
 	}
+
+	//Write to new file
+	let fullCode = beauty(config(name, version, perm, credit, desc, group, usage, cooldown, dependencies) + '\n\n' + run(message), { indent_size: 2, space_in_empty_paren: true })
+	fs.writeFileSync(`dist/${name}.js`, fullCode);
+	return console.log(`${chalk.green.bold('>>>')} ${chalk.green.underline.bold(`Wrote to dist/${name}.js.`)} ${chalk.green.bold('<<<')}`);
 };
 init();

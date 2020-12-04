6948c4388b0fe8bca19e0e76d6e496df3152ca6b
SpermLord <hiimnghia.real@gmail.com>
2020-12-04 13:34:34 +0700

    0.0.6

--- README.md
+++ README.md
@@ -1,23 +1,13 @@
 # Convertv2
 
-> This project will convert a command from Mirai v1 to Mirai v2
+> Repo này sẽ chuyển đổi các lệnh từ Miraiv1 sang Miraiv2
 
-> WIP
+## Cách dùng
 
-## How To Use
+##### 1. Copy file 3 file sau từ bản Miraiv1 vào thư mục lib:
++ app/handle/message.js
++ app/handle/src/help/listCommands.json
++ app/handle/src/lang/vi.lang
 
-- Open cmd in this project folder.
-
-- Type this in cmd:
-```cmd
-mklink /d "v1" "<Path To V1>"
-```
-
-Replace <Path To V1> with the actual path. Eg: C:\Users\SpermLord\Desktop\mirai
-
-- Use:
-```
-node index <command>
-```
-
-Eg: ```node index help```
+##### 2. Mở Command Promt/Terminal trong thư mục này.
+##### 3. Gõ ```node index``` để sử dụng.
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const fs = require('fs-extra');
 const beauty = require('js-beautify').js;
 const getCmd = require('./lib/cmd');
 const getText = require('./lib/text');
+const makeInfo = require('./lib/info');
+const getContent = require('./lib/content');
 
 //Clear terminal/command promt interface and print Convertv2
 clear();
@@ -16,70 +18,34 @@ console.log(chalk.cyan('> By SpermLord'));
 fs.ensureDirSync('dist');
 
 //message.js
-var message = fs.readFileSync('v1/app/handle/message.js', { encoding: 'utf-8' });
-var helpInfo = JSON.parse(fs.readFileSync('v1/app/handle/src/help/listCommands.json'));
-
-//Run holder
-var run = (code) => `module.exports.run = function({ api, event, args, client, __GLOBAL }) {
-${code}
-}`;
-
-//Config holder
-var config = (name, ver, perm, credit, desc, group, usage, cooldown, dependencies, info) => `module.exports.config = {
-name: "${name}",
-version: "${ver}",
-hasPermssion: ${perm},
-credits: "${credit}",
-description: "${desc}",
-commandCategory: "${group}",
-usages: "${usage}",
-cooldowns: ${cooldown},
-dependencies: ${dependencies}
-};`
+var message = fs.readFileSync('lib/message.js', { encoding: 'utf-8' });
 
 //Do the thing
 let init = async () => {
-	var hasCB = 0;
-
 	//Wait for CLI
 	const credentials = await getCmd.askCmdInfo(message);
 	let { name, version, perm, credit, cooldown, dependencies } = credentials;
 
-	//Get if/else content
-	let checkIf = message.indexOf(`if (contentMessage.indexOf(\`\$\{prefix\}${name}\`) == 0`) || message.indexOf(`if (contentMessage == \`\$\{prefix\}${name}\``);
-	message = message.slice(checkIf, message.length);
-	let getComment = message.search(/[^\:]\/\/.*/);
-	message = message.slice(0, getComment);
-	let start = message.search(/if \(.*\) \{/) + 3 || message.search(/if \(.*\)\{/) + 2 || message.indexOf('return');
-	message = message.replace(/senderID/g, 'event.senderID').replace(/, threadID/g, ', event.threadID').replace(/, messageID/g, ', event.messageID').replace(/\t/g,'').replace(/content \= contentMessage.slice\(prefix.length \+ \d, contentMessage.length\)/, 'content \= args.join(" ")').replace(/contentMessage\.length/g, 'content.length');
-	let splitMessage = message.split(/\r?\n/);
-	if (splitMessage[0].includes(') {') || splitMessage[0].includes('){')) hasCB = 1;
-	splitMessage.splice(0, 1);
-	splitMessage = splitMessage.filter(i => i != '');
-	if (hasCB == 1) splitMessage = splitMessage.slice(0, -1);
-	message = splitMessage.join('\n');
-
 	//Get cmd info
-	let cmdInfo = helpInfo.filter(item => item.name == name)[0];
-	let { desc, usage, group } = cmdInfo;
-	let splitDependencies = dependencies.split(', ');
-	if (splitDependencies.length > 0 && splitDependencies[0] != '') dependencies = JSON.stringify(splitDependencies);
-	else dependencies = '[]';
+	let config = makeInfo(name, version, perm, credit, cooldown, dependencies);
+
+	//Get if/else content
+	let content = getContent(message, name);
 
 	//Reverse getText function
-	let a = message.match(/api.sendMessage\(getText(.*?)ID\)/g);
-	if (a) {
-		for (let i of a) {
-			let o = i;
+	let reverse = content.match(/api.sendMessage\(getText(.*?)ID\)/g);
+	if (reverse) {
+		for (let i of reverse) {
+			let original = i;
 			i = i.replace(/api\.sendMessage\(/g, '').replace(/, event\.threadID, event\.messageID\)/g, '').replace(/getText\(/g, '');
 			i = i.slice(0, -1);
 			let send = (text) => `api.sendMessage(\`${text}\`, event.threadID, event.messageID)`;
-			message = message.replace(o, send(getText(i)));
+			content = content.replace(original, send(getText(i)));
 		}
 	}
 
 	//Write to new file
-	let fullCode = beauty(config(name, version, perm, credit, desc, group, usage, cooldown, dependencies) + '\n\n' + run(message), { indent_size: 2, space_in_empty_paren: true })
+	let fullCode = beauty(config + '\n\n' + content, { indent_size: 2, space_in_empty_paren: true })
 	fs.writeFileSync(`dist/${name}.js`, fullCode);
 	console.log(`${chalk.green.bold('>>>')} ${chalk.green.underline.bold(`Đã ghi vào dist/${name}.js.`)} ${chalk.green.bold('<<<')}`)
 	return console.log(`${chalk.red.bold('>>>')} ${chalk.red.bold(`Bạn sẽ cần phải check lại file 1 lần nữa`)} ${chalk.red.bold('<<<')}`);;
